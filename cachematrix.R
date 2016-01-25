a3b58d37deb934cfc9add7090e4631bb09d89426
Adolfo Correa <fitomillennium@hotmail.com>
2016-01-25 02:13:02 -0200

    Inverse Matrix 3 functions: 	1st) make an object that save a matrix and its inverse 	2nd) calculate the inverse of matrix 	3rd) test the above functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv) m <<- inv
+    getinv <- function() m
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+##  This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
+# Computing the inverse of a square matrix can be done with the solve function in R. For example, if X is a square invertible matrix, then solve(X) returns its inverse.
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    
+    m <- x$getinv()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)
+    m
 }
+
+# This function test if this program works
+# Comparing the m %*% m^-1 with identity matrix
+testing <- function(){
+    mat <- matrix(1:4, 2, 2)
+    mat1 <- makeCacheMatrix(mat)
+    mat1_inv <- cacheSolve(mat1)
+    
+    if( (mat1$get() %*% mat1_inv) == matrix(c(1, 0, 0, 1), 2, 2)){
+        print("sucess")
+        TRUE
+    }
+         
+    
+}
+
